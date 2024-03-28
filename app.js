612f9e0942f876a388fb2c6bad86390ddc931d17
PortelaZod <jeffersonportela@live.com>
2024-03-28 03:32:40 -0300

    1.0

---
+++ app.js
@@ -0,0 +1,16 @@
+const barraLinks= document.getElementsByClassName("btn-bar")[0]
+const btnAbrir=document.getElementById("open-btn")
+const btnFechar=document.getElementById("close-btn")
+
+function abrir(){
+    barraLinks.style.left="0"
+    btnAbrir.style.display="none"
+    btnFechar.style.display="flex"
+
+}
+
+function  fechar(){
+    barraLinks.style.left="-100%"
+    btnAbrir.style.display="flex"
+    btnFechar.style.display="none"
+}
